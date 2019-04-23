05450de1910163ad42b3cdabfcd411f6cd7ea26f
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-23 14:05:00 -0400

    0.1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Client.GameOver.MixProject do
   def project do
     [
       app: :islands_client_game_over,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands Client Game Over",
